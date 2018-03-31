18a49d77c1eb31734ab34383b67f0720a5a39108
Marian Domanski <mdomanski@usgs.gov>
2018-03-30 21:41:39 -0500

    Added setUp() to TestEZQADVMDataInit

--- test/test_nortek.py
+++ test/test_nortek.py
@@ -4,20 +4,24 @@ import unittest
 import numpy as np
 import pandas as pd
 
-from advmdata.nortek import EzqADVMData
+from advmdata.nortek import EZQADVMData
 
 
-class TestEzqADVMData(unittest.TestCase):
-    """Test the public methods of the EzqADVMData class"""
+class TestEZQADVMDataInit(unittest.TestCase):
+    """Test the successful initialization of the EZQADVMData class"""
+
+    def setUp(self):
+        """Initialize an instance of EZQADVMData to test"""
+
+        self.data_set_path = os.path.join('data', 'nortek', 'EZQ', 'EZQ')
+        self.ezq_advm_data = EZQADVMData.read_ezq_data(self.data_set_path, cell_size=0.4)
 
     def test_read_ezq_data(self):
-        """Test the read_ezq_data method and compare it to the expected results"""
+        """Test the read_ezq_data method and compare the relevant acoustic data to the expected results"""
 
-        data_set_path = os.path.join('data', 'nortek', 'EZQ', 'EZQ')
-        ezq_advm_data = EzqADVMData.read_ezq_data(data_set_path)
-        read_results_df = ezq_advm_data.get_data()
+        read_results_df = self.ezq_advm_data.get_data()
 
-        results_path = data_set_path + '_results.txt'
+        results_path = self.data_set_path + '_results.txt'
         expected_results_df = pd.read_table(results_path, index_col=0, parse_dates=True)
 
         # test all times are being read correctly
@@ -36,9 +40,9 @@ class TestEzqADVMData(unittest.TestCase):
         self.assertTrue(np.all(column_results_series))
 
         # test the origin is created correctly
-        read_results_origin = ezq_advm_data.get_origin()
+        read_results_origin = self.ezq_advm_data.get_origin()
 
-        expected_origin_path = data_set_path + '_expected_origin.txt'
+        expected_origin_path = self.data_set_path + '_expected_origin.txt'
         expected_origin = pd.read_table(expected_origin_path, index_col=0)
 
         self.assertTrue(np.all(read_results_origin == expected_origin))
